24ea1f8ce3a570cc49e6d52230d6ab0c7c6d31a7
Gersomsim <gersomhernandez@me.com>
2023-04-19 20:25:05 -0600

    :art: Estructura del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const tofod = [
+    { text: 'cortar cebolla', complete: false },
+    { text: 'Tomar curso de intro de react', complete: false },
+    { text: 'Llorar con la llorona', complete: false }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+          <TodoCounter />
+          <h2>Haz completado 3 de 4 TODOs</h2>
+          <TodoSearch />
+          <input type="text" placeholder="Cebolla"/>
+          <TodoList>
+              {todos.map(todo => (
+                  <TodoItem />
+              ))}
+          </TodoList>
+          <CreateTodoButton />
+          <button>+</button>
+      </React.Fragment>
   );
 }
 
